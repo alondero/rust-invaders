cf44d660609e66cba0785f20759dd46823a1b0be
Adam Londero <alondero@gmail.com>
2016-05-21 20:38:07 +0000

    Includes SDL2 and renders a blank canvas in full screen. Hit escape to exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate sdl2;
+
+mod events;
+
+use events::Events;
+use sdl2::pixels::Color;
+
 fn main() {
-	println!("Hello, World!");
+	// Initialize SDL2
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    // Create the window
+    let window = video.window("Rust Invaders", 800, 600)
+        .position_centered()
+        .fullscreen()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer()
+        .accelerated()
+        .build()
+        .unwrap();
+
+    let mut events = Events::new(sdl_context.event_pump().unwrap());
+
+    loop {
+    	events.pump();
+
+    	if events.quit || events.key_escape {
+    		break;
+    	}
+    
+    	renderer.set_draw_color(Color::RGB(0, 0, 0));
+	    renderer.clear();
+    	renderer.present();
+    }
 }
