4e9b09fdf0d06bb5a422f9d92564c9efb57ab166
Adam Londero <alondero@gmail.com>
2016-05-23 19:54:55 +0000

    Extracting view logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate sdl2;
 
 mod engine;
+mod views;
 
 use sdl2::pixels::Color;
-use engine::Events;
+use engine::{Events, Engine, View, ViewAction};
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -15,22 +16,19 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut renderer = window.renderer()
-        .accelerated()
-        .build()
-        .unwrap();
+    let mut context = Engine {
+    	events: Events::new(sdl_context.event_pump().unwrap()),
+    	renderer: window.renderer().accelerated().build().unwrap()
+    };
 
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
+    let mut current_view: Box<View> = Box::new(::views::DefaultView);
 
     loop {
-    	events.pump();
+    	context.events.pump();
 
-    	if events.now.quit || events.now.key_escape == Some(true) {
-    		break;
+    	match current_view.render(&mut context, 0.01) {
+    		ViewAction::None => context.renderer.present(),
+    		ViewAction::Quit => break
     	}
-    
-    	renderer.set_draw_color(Color::RGB(0, 0, 0));
-    	renderer.clear();
-    	renderer.present();
     }
 }
